be907ffc2803ba2eee966ffc95ecbc81b1220a12
jeevaf22 <48169919+Jeeva-ss@users.noreply.github.com>
2023-03-29 02:12:05 +0530

    list

--- src/components/List.js
+++ src/components/List.js
@@ -2,7 +2,7 @@ export default function List({ userData, loading, error }) {
   return (
     <div className='w-11/12 drop-shadow-2xl md:w-5/12 bg-white mx-auto text-center text-slate-600 rounded-2xl border'>
       <p className='py-8 bg-zinc-200 text-xl font-bold rounded-t-2xl'>Select an account</p>
-      <ul role="list" className="p-6 m-4 rounded-b-2xl bg-white divide-y divide-slate-200 h-80 overflow-y-scroll overflow-x-hidden">
+      <ul className="p-6 m-4 rounded-b-2xl bg-white divide-y divide-slate-200 h-80 overflow-y-scroll overflow-x-hidden">
         {userData.map((user) => (
           <li className="flex items-center py-4 first:pt-0 last:pb-0" key={user.id}>
             <img className="h-10 w-10 rounded-full" src={user.profilepicture} alt="user" />
